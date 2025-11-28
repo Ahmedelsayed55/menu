42f252dd5ce7845d91bb3c80882c338ab25b35ac
Ahmedelsayed55 <am1096922@gmail.com>
2025-11-28 16:33:19 +0200

    v-5

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -6,9 +6,9 @@ const NavBar = () => {
     <div className='container mx-auto'>
         <header className="flex justify-between items-center py-4 rounded-b-2xl shadow-yellow-700 shadow-md md:px-5" >
           <nav className="flex gap-1 md:gap-3">
-            <Link className='text-[14px] md:text-2xl p-3 border-b-0 transition  hover:border-b-2 border-yellow-700' to="/">الرئيسية</Link>
-            <Link className='text-[14px] md:text-2xl p-3 border-b-0 transition  hover:border-b-2 border-yellow-700' to="/contact">تواصل معنا</Link>
-            <Link className='text-[14px] md:text-2xl p-3 border-b-0 transition  hover:border-b-2 border-yellow-700' to="/about">المزيد عنا </Link>
+            <Link className='text-[14px] md:text-[17px] p-3 border-b-2 border-yellow-900/0 hover:border-b-2 hover:border-b-yellow-700' to="/">الرئيسية</Link>
+            <Link className='text-[14px] md:text-[17px] p-3 border-b-2 border-yellow-900/0 hover:border-b-2 hover:border-b-yellow-700' to="/contact">تواصل معنا</Link>
+            <Link className='text-[14px] md:text-[17px] p-3 border-b-2 border-yellow-900/0 hover:border-b-2 hover:border-b-yellow-700' to="/about">المزيد عنا </Link>
           </nav>
           <img className="w-15 h-12 md:w-18 md:h-15" src={logo} alt="Logo" />
         </header>
