63138c3e1aa36de84c9a1a6958daa749d6574610
Ahmedelsayed55 <am1096922@gmail.com>
2025-11-30 15:26:05 +0200

    v-34

--- src/Component/MenuComponent/Cake26.jsx
+++ src/Component/MenuComponent/Cake26.jsx
@@ -48,14 +48,14 @@ const Cake26 = ({ id }) => {
           return (
             <div
               key={item.id}
-              className="rounded-sm h-70 md:h-120 shadow-2xl shadow-gray-400 pt-15 px-1 flex flex-col items-center justify-between transition hover:scale-105 bg-gray-100 active:scale-110 focus:scale-110"
+              className="rounded-sm h-70 md:h-120 shadow-2xl shadow-gray-400 pt-10 md:pt-15 px-1 flex flex-col items-center justify-between transition hover:scale-105 bg-gray-100 active:scale-110 focus:scale-110"
             >
-              <img className=" w-full md:w-60  md:mb-10 transition filter hover:drop-shadow-[0_10px_5px_rgba(102,231,239,0.2)]" src={item.img} alt="Cake" />
-              <div className="w-full flex flex-col gap-7 md:gap-15 py-3 px-1">
-                <h2 className="text-start text-[14px] md:text-[20px] font-bold text-gray-900">
+              <img className=" w-full md:w-60  md:mb-7 transition filter hover:drop-shadow-[0_10px_5px_rgba(102,231,239,0.2)]" src={item.img} alt="Cake" />
+              <div className="w-full flex flex-col gap-7 md:gap-15 py-3 px-3">
+                <h2 className="text-start text-[14px] md:text-[24px] font-bold text-cyan-950">
                   {item.name}
                 </h2>
-                <h2 className="text-start text-[12px] md:text-[18px] font-bold text-gray-700">
+                <h2 className="text-start text-[12px] md:text-[20px] font-bold text-cyan-700">
                   {item.price} ج.م
                 </h2>
               </div>
--- src/Component/MenuComponent/Gato.jsx
+++ src/Component/MenuComponent/Gato.jsx
@@ -22,7 +22,7 @@ export const Gato = ({id}) => {
       ]
   return (
     <div>
-      <h1 className="text-[16px] font-bold md:text-2xl mb-5">جاتو </h1>
+      <h1 className="text-[16px] font-bold md:text-2xl mb-5">تورته مقاس (26)</h1>
       <div
         className=" grid grid-cols-2 md:grid-cols-3  lg:grid-cols-5 gap-2 md:gap-5"
         id={id}
@@ -31,14 +31,14 @@ export const Gato = ({id}) => {
           return (
             <div
               key={item.id}
-              className="rounded-sm shadow-2xl shadow-gray-400 pt-15 px-1 flex flex-col gap-5 items-center transition hover:scale-105 bg-gray-100 active:scale-110 focus:scale-110"
+              className="rounded-sm h-70 md:h-120 shadow-2xl shadow-gray-400 pt-10 md:pt-15 px-1 flex flex-col items-center justify-between transition hover:scale-105 bg-gray-100 active:scale-110 focus:scale-110"
             >
-              <img className=" w-full md:w-[85%]  md:mb-10 transition filter hover:drop-shadow-[0_10px_5px_rgba(102,231,239,0.2)]" src={item.img} alt="Cake" />
-              <div className="w-full flex flex-col gap-7 py-3">
-                <h2 className="text-start text-[14px] me:text-[20px] font-bold">
+              <img className=" w-full md:w-60  md:mb-7 transition filter hover:drop-shadow-[0_10px_5px_rgba(102,231,239,0.2)]" src={item.img} alt="Cake" />
+              <div className="w-full flex flex-col gap-7 md:gap-15 py-3 px-3">
+                <h2 className="text-start text-[14px] md:text-[24px] font-bold text-cyan-950">
                   {item.name}
                 </h2>
-                <h2 className="text-start text-[12px] md:text-[18px] font-bold text-gray-800">
+                <h2 className="text-start text-[12px] md:text-[20px] font-bold text-cyan-700">
                   {item.price} ج.م
                 </h2>
               </div>
