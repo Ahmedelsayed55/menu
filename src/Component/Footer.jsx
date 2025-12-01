5025d83ffa401d947c85a5105cfe0cd6cd1f70f1
Ahmedelsayed55 <am1096922@gmail.com>
2025-12-01 22:49:46 +0200

    v-40

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -2,19 +2,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" flex justify-center items-center py-20 bg-gray-200">
-      <h2 className="text-cyan-900 text-2xl text-center flex flex-row-reverse items-center gap-1">
-        {" "}
-        :Created by Ahmed Elsayed © 2025 Phone & Whatsapp
-        <a
-          className="flex text-[20px] mb-3 md:mb-0 md:text-[24px] font-bold gap-1 items-center transition hover:text-cyan-800 hover:scale-102"
-          href="https://wa.me/201016223981?"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          01016223981
-        </a>
-      </h2>
+    <div className=" flex justify-center  items-center py-10 bg-gray-200">
+      <a
+        className=" text-[20px] mb-3 md:mb-0 text-center md:text-[24px] font-bold text-cyan-900   transition hover:text-cyan-800 hover:scale-102"
+        href="https://wa.me/201016223981?"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Created by Ahmed Elsayed © 2025 Phone & Whatsapp : 01016223981
+      </a>
     </div>
   );
 };
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -4,38 +4,43 @@ import { Link } from "react-router-dom";
 import { FaInstagram, FaFacebook, FaWhatsapp } from "react-icons/fa";
 const Home = () => {
   return (
-    <div className="bg-gray-200 py-10 md:py-20 lg:py-30 flex items-center justify-center ">
+    <div className="bg-gray-200 pt-20 pb-10 md:py-15 lg:py-32 flex items-center justify-center ">
       <div className="container mx-auto ">
         <div className=" flex items-center justify-center rounded-2xl">
-          <div className="flex flex-col items-center gap-5">
-            <img className="w-60" src={logo} alt="Logo" />
-            <h1 className="text-3xl font-bold text-center mb-3">
+          <div className="flex flex-col items-center  gap-10">
+            <img className="w-30 md:w-40 lg:w-60" src={logo} alt="Logo" />
+            <h1 className="text-2xl md:text-3xl font-bold text-center my-3 text-cyan-800">
               حلواني ريتاج-Retaj pastry
             </h1>
-            <Link className="btn btn-warning p-5 text-2xl" to="/menu">
-              قائمة الاسعار
-            </Link>
-            <Link className="btn btn-warning p-5 text-2xl" to="/contact">
-              للتواصل معنا
-            </Link>
-            <div className="flex gap-5 mt-5">
-              <a href="https://www.facebook.com/retajpastry" target="_blank" className=" hover:scale-125 transition-transform duration-300">
-                <FaFacebook className="text-4xl text-yellow-500" />
+            <div className="flex flex-col gap-5">
+              <Link
+                className="btn bg-cyan-800 px-20 md:px-40  py-6 rounded-3xl text-2xl"
+                to="/menu"
+              >
+                قائمة الاسعار
+              </Link>
+              <Link
+                className="btn bg-cyan-800 px-20 md:px-40  py-6 rounded-3xl text-2xl"
+                to="/contact"
+              >
+                للتواصل معنا
+              </Link>
+
+            </div>
+            <div className="flex gap-5 ">
+              <a
+                href="https://www.facebook.com/retajpastry"
+                target="_blank"
+                className=" hover:scale-125 transition-transform duration-300"
+              >
+                <FaFacebook className="text-4xl text-cyan-800 hover:text-cyan-600" />
               </a>
               <a
                 href="https://www.instagram.com/retajpastry"
                 target="_blank"
                 className=" hover:scale-125 transition-transform duration-300"
               >
-                <FaInstagram className="text-4xl text-yellow-500" />
-              </a>
-              <a
-                href="https://wa.me/201090222129?text=Hello"               
-                target="_blank"
-                rel="noopener noreferrer"
-className=" hover:scale-125 transition-transform duration-300"
-              >
-                <FaWhatsapp className="text-4xl text-yellow-500" />
+                <FaInstagram className="text-4xl text-cyan-800 hover:text-red-400" />
               </a>
             </div>
           </div>
