3794c14291837ad2d195670a4fff6e4d812a63e0
Ahmedelsayed55 <am1096922@gmail.com>
2025-12-13 11:18:32 +0200

    n

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { ImHome } from "react-icons/im";
+import { FiPhoneCall } from "react-icons/fi";
 const NavBar = () => {
   return (
-    <div className="container  absolute top-0 left-1/2 -translate-x-1/2 z-50 ">
-      <header className="flex justify-between items-center px-1.5  md:px-5 py-2 md:py-5 backdrop-blur-2xl bg-gray-400/10 rounded-b-2xl">
-        <nav className="flex gap-1 md:gap-3">
+    <div className="container mx-auto ">
+      <header className="flex justify-between items-center px-3  md:px-7 py-2 md:py-5 ">
+        <nav className="flex gap-10 md:gap-20">
           <Link
-            className="text-[14px] text-yellow-600 md:text-[20px] p-3 border-b-2 border-yellow-900/0 hover:border-b-2 hover:border-b-yellow-600"
+            className="text-2xl md:text-4xl text-yellow-600 transition-all hover:text-white"
             to="/"
           >
-            الرئيسية
+            <ImHome />
           </Link>
           <Link
-            className="text-[14px] text-yellow-600 md:text-[20px] p-3 border-b-2 border-yellow-900/0 hover:border-b-2 hover:border-b-yellow-600"
+            className="text-2xl md:text-4xl text-yellow-600 transition-all hover:text-white"
             to="/contact"
           >
-            تواصل معنا
+            <FiPhoneCall />
           </Link>
         </nav>
         <img className="w-12 h-10 md:w-19 md:h-15" src={logo} alt="Logo" />
