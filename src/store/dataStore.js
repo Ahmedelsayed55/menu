d9e7de4b542cec0c8dcfe763fec90034f2883e44
Ahmedelsayed55 <am1096922@gmail.com>
2026-01-04 15:38:10 +0200

    v-create file store data

---
+++ src/store/dataStore.js
@@ -0,0 +1,26 @@
+import galaxy from "../assets/assets26/a1.png";
+import snekers from "../assets/assets26/a10.png";
+export const allProducts = [
+  {
+    id: 1,
+    section: "تورته 26سم",
+    name: "تورته جلاكسي 26سم",
+    price: 350,
+    img: galaxy,
+  },
+  {
+    id: 2,
+    section: "تورته 26سم",
+    name: "تورته سنيكرز 26سم",
+    price: 350,
+    img: snekers,
+  },
+
+  {
+    id: 3,
+    section: "تورته 20سم",
+    name: "تورته جلاكسي 20سم",
+    price: 300,
+    img: galaxy,
+  },
+];
