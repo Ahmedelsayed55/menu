a8f3f7692523b82f8cea0cac5cbfa7f9a3c2c007
Ahmedelsayed55 <am1096922@gmail.com>
2026-01-22 09:35:34 +0200

    img url

--- src/store/dataStore.js
+++ src/store/dataStore.js
@@ -1,27 +1,59 @@
-import galaxy from "../assets/assets26/a1.png";
-import snekers from "../assets/assets26/a10.png";
+// قسم التورته 26سم
+import havGlaxyChocolata from "../assets/assets26/havGlaxyChocolata.png";
+import Cake1 from "../assets/assets26/Cake1.png";
+import Cake2 from "../assets/assets26/Cake2.png";
+import Cake3 from "../assets/assets26/Cake3.png";
+import Cake4 from "../assets/assets26/Cake4.png";
+import Cake7 from "../assets/assets26/Cake7.png";
+import notelaOrio from "../assets/assets26/notelaOrio.png";
+import Cake11 from "../assets/assets26/Cake11.png";
+import chocolate from "../assets/assets26/chocolate.png";
+import havTag from "../assets/assets26/havTag.png";
+import Cake15 from "../assets/assets26/Cake15.png";
+import Cake16 from "../assets/assets26/Cake16.png";
+import Cake17 from "../assets/assets26/Cake17.png";
+import snecars from "../assets/assets26/snecars.png";
+import karamilBlack from "../assets/assets26/karamilBlack.png";
+import whiteMillk from "../assets/assets26/whiteMillk.png";
+import flwar from "../assets/assets26/flwar.png";
+
+// قسم التورته 20سم
+// قسم التورته 20سم
+// قسم التورته 20سم
+
 export const allProducts = [
   // قسم التورته 26سم
   {
     id: 1,
-    section: "تورته 26سم",
-    name: "تورته جلاكسي 26سم",
+    name: "تورته هاف جلاكسي شيكولاته ",
     price: 350,
-    img: galaxy,
+    img: havGlaxyChocolata,
   },
+  { id: 2, name: "تورته فلاور ", price: 300, img: Cake1 },
+  { id: 3, name: "تورته هاف لوتس & شيكولاته ", price: 350, img: Cake2 },
+  { id: 4, name: "تورته جلاكسي ", price: 350, img: Cake3 },
+  { id: 5, name: "تورته نوتيلا فراوله ", price: 350, img: Cake4 },
+  { id: 6, name: "تورته هاف كراميل & شيكولاته ", price: 300, img: Cake7 },
+  { id: 7, name: "تورته نوتيلا اوريو ", price: 350, img: notelaOrio },
+  { id: 8, name: "تورته ايت سيزون اسبونج ", price: 400, img: Cake11 },
+  { id: 9, name: "تورته شيكولاته ", price: 300, img: chocolate },
+  { id: 10, name: "تورته هاف تاج ", price: 300, img: havTag },
+  { id: 11, name: "تورته فلوتس ", price: 375, img: Cake15 },
+  { id: 12, name: "تورته هاف جلاكسي & فاكهة ", price: 375, img: Cake16 },
+  { id: 13, name: "تورته فور سيزون فادج ", price: 375, img: Cake17 },
+  { id: 14, name: "تورته سنيكرز ", price: 350, img: snecars },
   {
-    id: 2,
-    section: "تورته 26سم",
-    name: "تورته سنيكرز 26سم",
-    price: 350,
-    img: snekers,
-  },
-
-  {
-    id: 3,
-    section: "تورته 20سم",
-    name: "تورته جلاكسي 20سم",
+    id: 15,
+    name: "تورته هاف كارميل & بلاك فورست",
     price: 300,
-    img: galaxy,
+    img: karamilBlack,
   },
+  { id: 16, name: "تورته وايت فورست & حليب", price: 350, img: whiteMillk },
+  { id: 17, name: "تورته ريد فالفيت", price: 350, img: "" },
+  { id: 18, name: "تورته تشيز كيك", price: 350, img: "" },
+  { id: 18, name: " تورته فلاور  ", price: 350, img: flwar },
+
+  // قسم التورته 20سم
+  // قسم التورته 20سم
+  // قسم التورته 20سم
 ];
